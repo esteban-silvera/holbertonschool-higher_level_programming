fd44f458e4201efedaad28725ea57ba5803c859d
esteban-silvera <estebansilvera000@gmail.com>
2023-09-22 19:54:07 -0300

    all

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for letra in range(97, 123):
-    print(chr(letra), end='')
+    print("{}".format(chr(letra)), end='')
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letra in range(97, 123):
     if chr(letra) not in "e" and chr(letra) not in "q":
-        print(f"{chr(letra)}", end='')
+        print("{}".format(f"{chr(letra)}"), end='')
