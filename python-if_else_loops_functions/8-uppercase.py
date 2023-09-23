4976635d26bc76a5e0bc20ea66834b1ed5d3759e
esteban-silvera <estebansilvera000@gmail.com>
2023-09-23 04:01:18 -0300

    .\8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    texto = ""
     for letra in str:
         if 97 <= ord(letra) <= 122:
-            texto += chr(ord(letra) - 32)
-        else:
-            texto += letra
-    print(f"{texto}", end="")
+            letra = chr(ord(letra) - 32)
+        print("{}".format(letra), end="")
+    print("")
