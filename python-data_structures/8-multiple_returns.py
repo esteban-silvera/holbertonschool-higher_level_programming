9837dfcfc7e15e2a3cf78d94b978e466e4d764ec
esteban-silvera <estebansilvera000@gmail.com>
2023-09-24 20:59:45 -0300

    .\8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     if sentence == None:
         return None
     x = len(sentence)
-    print("Length: {} - First character: {}".format(x, sentence[0]))
+    first = sentence[0]
+    return (x, first)
