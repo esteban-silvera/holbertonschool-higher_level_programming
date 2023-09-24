e1f4e973a56202b00523ce0f665a3133cebe2c53
esteban-silvera <estebansilvera000@gmail.com>
2023-09-24 19:59:45 -0300

    .\7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    nuevo = 0
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + nuevo
+    if len(tuple_b) < 2:
+        tuple_b = tuple_a + nuevo
+
+    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result_tuple
+    
