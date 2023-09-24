8d85b123e03c94a825dc6146f19d780677378f14
esteban-silvera <estebansilvera000@gmail.com>
2023-09-23 23:55:04 -0300

    .\3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    largo = len(argv) - 1
     total = 0
-    for x in range(len(argv) - 1):
+    for x in range(largo):
         total += int(argv[x + 1])
     print("{}".format(total))
