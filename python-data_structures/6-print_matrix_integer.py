e1be81271759348ceb33324215434624b15717b0
esteban-silvera <estebansilvera000@gmail.com>
2023-09-24 19:22:01 -0300

    .\6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,10 +4,3 @@ def print_matrix_integer(matrix=[[]]):
         for valor in fila:
             print("{:d}".format(valor), end=" ")
         print()
-
-ejemplo = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(ejemplo)
