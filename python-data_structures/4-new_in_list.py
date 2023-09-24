7f3614f086d1229ff5399d5a21085431dc87cb55
esteban-silvera <estebansilvera000@gmail.com>
2023-09-24 17:51:05 -0300

    .\4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    lista2 = my_list
+    lista2 = [x for x in my_list]
     if idx < 0 or idx > (len(my_list) - 1):
         return lista2
     lista2[idx] = element
