1ea6a4fccf60e916ee457d70e482650ca9738849
esteban-silvera <estebansilvera000@gmail.com>
2023-09-24 21:31:27 -0300

    .\10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     listarda = []
+    x = 0
     for nums in my_list:
         if nums % 2 == 0:
-            listarda[nums] = True
+            listarda[x] = True
+            x + 1
         else:
-            listarda[nums] = False
+            listarda[x] = False
+            x + 1
         return listarda
