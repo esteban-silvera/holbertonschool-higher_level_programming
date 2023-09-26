a61f44e0908e5fd0a75dbf30390d0544f96c3c86
esteban-silvera <estebansilvera000@gmail.com>
2023-09-26 12:53:58 -0300

    .\1-search_replace.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -7,3 +7,13 @@ def square_matrix_simple(matrix=[]):
             new_fila.append(valor * valor)
         matrix.append(new_fila)
     return matrix2
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
