0f88018dfcb3ef9f73dbb701f0d5f76cd6b699b6
esteban-silvera <estebansilvera@gmail.com>
2023-09-27 12:40:55 +0000

    3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -5,7 +5,3 @@ def common_elements(set_1, set_2):
         if elemento in set_2:
             lista.append(elemento)
     return lista
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
