65094de4933db1c1802bd1dc6471eaf936f12e8e
esteban-silvera <estebansilvera@gmail.com>
2023-09-27 12:24:57 +0000

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    listi = my_list
+    listi = []
     for num in my_list:
         if num == search:
-            listi[num] = replace
+            listi.append(replace)
+        else:
+            listi.append(num)
     return listi
