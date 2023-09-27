f51c63c6639cc126fdeb98c002f22262175a42f6
esteban-silvera <estebansilvera@gmail.com>
2023-09-27 14:12:43 +0000

    8-simple_delete.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary:
-        return a_dictionary
     for format in a_dictionary:
         if format == key:
             del(a_dictionary[key])
