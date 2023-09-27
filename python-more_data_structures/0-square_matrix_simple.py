d4cc8351721d627fef799624235fa67c22605ea2
esteban-silvera <estebansilvera@gmail.com>
2023-09-27 12:20:34 +0000

    0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -7,13 +7,3 @@ def square_matrix_simple(matrix=[]):
             new_fila.append(valor * valor)
         matrix2.append(new_fila)
     return matrix2
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
