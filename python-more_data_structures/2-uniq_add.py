874e5493025fbd85c7631df4380f49f5922bf261
esteban-silvera <estebansilvera@gmail.com>
2023-09-27 12:34:22 +0000

    2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    lista = []
     suma = 0
     for num in my_list:
-        suma += num
+        if num not in lista:
+            lista.append(num)
+            suma += num
     return suma
