14a9c5ac8678ed5febb73cf63ee42e6943f79633
esteban-silvera <estebansilvera000@gmail.com>
2023-09-29 08:53:36 -0300

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        x = False
         if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
+            x = True
+        print("{:d}".format(value))
+        return x
     except (ValueError, TypeError):
-        return False
-    finally:
-        print("")
+        return x
