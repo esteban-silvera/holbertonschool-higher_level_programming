08b3b13e741eb747b40250535f3d69a601b4a0ee
esteban-silvera <estebansilvera000@gmail.com>
2023-09-29 09:19:11 -0300

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if isinstance(i, int):
-                print("{}".format(my_list[i]), end="")
+                print("{:d}".format(my_list[i]), end="")
     except IndexError:
         return i
     else:
