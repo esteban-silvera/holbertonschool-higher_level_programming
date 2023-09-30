f7a3fb7161026cc46ed74da8be26d4daaa6e8872
esteban-silvera <estebansilvera000@gmail.com>
2023-09-30 19:41:48 -0300

    a

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+	Module 0-square
+	Defines class Square
+"""
+class Square:
+    """solo un cuadrado mi rey"""
+    pass
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def size(self):
+        tamaño = self.__size
+        return tamaño
+    
+    def area(self):
+        areovic = self.__size ** 2
+        return areovic
