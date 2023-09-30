64b129e9949755f29958f952f087a78fe553e7de
esteban-silvera <estebansilvera000@gmail.com>
2023-09-30 15:53:21 -0300

    .\1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+	Module 0-square
+	Defines class Square
+"""
+class Square:
+    """solo un cuadrado mi rey"""
+    pass
+    def __init__(self, size):
+        self.__size = size
