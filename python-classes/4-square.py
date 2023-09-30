72719a1cd846e8bf53294c0290cc31bd65beb594
esteban-silvera <estebansilvera000@gmail.com>
2023-09-30 19:48:53 -0300

    .\4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,18 +5,21 @@
 """
 class Square:
     """solo un cuadrado mi rey"""
+    
+    def __init__(self, size=0):
+        self.size = size
 
     def size(self):
         lado = self.__size
         return lado
 
-    def __init__(self, size=0):
-        if type(size) is not int:
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         areovic = self.__size ** 2
