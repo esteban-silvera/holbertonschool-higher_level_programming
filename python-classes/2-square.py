70630e4a5bdd390a71406c1b985021aad5bfb59a
esteban-silvera <estebansilvera000@gmail.com>
2023-09-30 19:31:25 -0300

    .\2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     pass
     def __init__(self, size=0):
         if size is int:
-            if size > 0:
+            if size < 0:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
