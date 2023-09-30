f31eecf5bfd4ad20788cf6e5275831d2908ad22b
esteban-silvera <estebansilvera000@gmail.com>
2023-09-30 19:33:10 -0300

    .\2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,10 +7,9 @@ class Square:
     """solo un cuadrado mi rey"""
     pass
     def __init__(self, size=0):
-        if size is int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
