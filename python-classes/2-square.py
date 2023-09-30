5a57dc8a3c44ea23d1367e49c0395b7a03f4e575
esteban-silvera <estebansilvera000@gmail.com>
2023-09-30 19:29:23 -0300

    .\2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+	Module 0-square
+	Defines class Square
+"""
+class Square:
+    """solo un cuadrado mi rey"""
+    pass
+    def __init__(self, size=0):
+        self.__size = size
+        if size is int:
+            if size > 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
