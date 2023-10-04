d8d25afd10bf87ba7518d630d495519d18ca7046
esteban-silvera <estebansilvera000@gmail.com>
2023-10-04 09:52:30 -0300

    2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+	Module 0-rectangle
+	Defines rectangle class
+"""
+class Rectangle:
+    """solo un rectangulo mi rey"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        ancho = self.__width
+        return ancho
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        alto = self.__height
+        return alto
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        aerovic = self.__height * self.__width
+        return aerovic
+
+    def perimeter(self):
+        nerimetro = 2 * (self.__height * self.__width)
+        return nerimetro
