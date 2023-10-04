f3a41486fc774f2f91e2e2bc0c51427c16d49667
esteban-silvera <estebansilvera000@gmail.com>
2023-10-04 14:26:35 -0300

    7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         else:
             rectangle_str = ""
             for _ in range(self.__height):
-                rectangle_str += str*(Rectangle.print_symbol) * self.__width + "\n"
+                rectangle_str += str(Rectangle.print_symbol) * self.__width + "\n"
             return rectangle_str[:-1] 
     
     def __del__(self):
