58577c04bb8a7f79d116e1a50e451e556331fa78
esteban-silvera <estebansilvera000@gmail.com>
2023-10-04 18:23:00 -0300

    8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -65,3 +65,17 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+
+        if area_1 >= area_2:
+            return rect_1
+        else:
+            return rect_2
