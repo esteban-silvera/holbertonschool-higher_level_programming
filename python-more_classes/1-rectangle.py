1b09c953fe9d0bf26831fe8ef0e1d025bdb42bbf
esteban-silvera <estebansilvera000@gmail.com>
2023-10-03 22:06:13 -0300

    .

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+	Module 0-rectangle
+	Defines rectangle class
+"""
+class Rectangle:
+    """solo un rectangulo mi rey"""
+    pass
+    def __init__(self, width=0):
+        self.width = width
+
+    def __init__(self, height=0):
+        self.height = height
+
+    def width(self):
+        ancho = self.__width
+        return ancho
+    
+    def width(self, value):
+
+    def height(self):
+        alto = self.height
+        return alto
+
+    def height(self, value):
