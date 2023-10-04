ec47b4d7ca674f1227205b6fe3def723c6b3ff2e
esteban-silvera <estebansilvera000@gmail.com>
2023-10-04 11:30:22 -0300

    6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
+        self.number_of_instances += 1
 
     @property
     def width(self):
@@ -61,3 +62,4 @@ class Rectangle:
     
     def __del__(self):
         print("Bye rectangle...")
+        self.number_of_instances -= 1
