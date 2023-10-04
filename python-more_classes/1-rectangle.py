1247f155e7dbd8c5b3ff31c8ba14818be648fbf4
esteban-silvera <estebansilvera000@gmail.com>
2023-10-04 09:20:44 -0300

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,8 +18,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.width = value
+        self.width = value
 
     def height(self):
         alto = self.height
@@ -30,5 +29,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.height = value
+        self.height = value
