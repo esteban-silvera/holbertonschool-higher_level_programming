25d3467f7de09e41e10549605cc55ed439a4fd31
esteban-silvera <estebansilvera000@gmail.com>
2023-10-07 17:26:29 -0300

    .

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""returns the list of available attributes and methods of an object"""
+
 def lookup(obj):
+    """Return list with objects available attributes"""
     return dir(obj)
