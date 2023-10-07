c380bd0c8be8f5bce6360e9e2d756d3a912e798b
esteban-silvera <estebansilvera000@gmail.com>
2023-10-07 19:54:46 -0300

    .\8-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle(BaseGeometry):
     
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
