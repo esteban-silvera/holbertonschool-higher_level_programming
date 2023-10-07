5bee67de5e0a95a4f1cb750ef84412241e1deab5
esteban-silvera <estebansilvera000@gmail.com>
2023-10-07 19:49:11 -0300

    .\8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """un objeto de base mi rey"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry  
+
 class Rectangle(BaseGeometry):
     """una base mi rey"""
+    
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
