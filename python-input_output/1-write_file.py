9581fb9791c098b1b6b1586113ef90bd3deb2d96
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 15:46:31 -0300

    .\1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,6 +2,8 @@
 """only a functions"""
 def write_file(filename="", text=""):
     """only define a functions"""
+    x = 0
     with open(filename, encoding="utf-8") as content:
-        content.write(text)
-        return len(text)
+        for line in content:
+            x += 1
+        return x
