023206755ecfdea0d7460af2a46ee00f171e9d5e
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 15:32:46 -0300

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""only a functions"""
+def write_file(filename="", text=""):
+    """only define a functions"""
+    with open(filename, encoding="utf-8") as fd:
+        filename += text
