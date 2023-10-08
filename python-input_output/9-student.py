c3c91dbe94a3e4073c012d41da8435e0cfcb2c9c
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 20:35:46 -0300

    .\9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,5 +7,5 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            return self.__dict__
+    def to_json(self):
+        return self.__dict__
