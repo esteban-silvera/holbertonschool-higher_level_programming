fde7774d18ab301f5e64e92a868e990b399dd118
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 15:35:36 -0300

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,5 +2,8 @@
 """only a functions"""
 def write_file(filename="", text=""):
     """only define a functions"""
-    with open(filename, encoding="utf-8") as fd:
-        filename += text
+    x = 0
+    with open(filename, encoding="utf-8") as text:
+        for letra in text:
+            x += 1
+        return x
