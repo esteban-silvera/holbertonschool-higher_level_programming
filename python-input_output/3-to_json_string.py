e269f1650f5b0452ea658d086c5e1785fd4188cb
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 17:31:37 -0300

    3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """only define a functions"""
+import json
+
 def to_json_string(my_obj):
     """only define a functions"""
-    import json
     json_str = my_obj
     return json_str
