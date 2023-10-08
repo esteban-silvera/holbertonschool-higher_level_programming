3a7ccbacb63fa7c8df8dea30b0fd0dd9ce2c0876
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 19:44:43 -0300

    .\7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,14 +4,10 @@ import json
 import sys
 import os
 save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-"""only define a functions"""
-
 load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
-"""only define a functions"""
 
 filename = "add_item.json"
 if not os.path.exists(filename):
-    """only define a functions"""
     save_to_json_file([], filename)
 my_list = load_from_json_file(filename)
 my_list.extend(sys.argv[1:])
