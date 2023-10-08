75752ed5cabbaccfe86a70a026a779c2a61a8ef3
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 19:05:31 -0300

    7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""only define a functions"""
+import json
+save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
