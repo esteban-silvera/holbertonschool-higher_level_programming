b0fbc339e740733f7c5041b3777f509525411de9
esteban-silvera <estebansilvera000@gmail.com>
2023-10-08 22:05:13 -0300

    .\11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""only define a functions"""
+class Student:
+    """only define a functions"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
