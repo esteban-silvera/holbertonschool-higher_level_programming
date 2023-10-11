9aba29555e414448701368ce6c76dba360f232e1
esteban-silvera <estebansilvera000@gmail.com>
2023-10-11 10:25:28 -0300

    1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,10 +2,10 @@
 """solo un rectangulo mi rey"""
 class Base:
     """solo un rectangulo mi rey"""
+    __nb_objects = 0
     def __init__(self, id=None):
-        self.__nb_objects = 0
         if id is not None:
-            self.__id = id
+            self.id = id
         else:
             type(self).__nb_objects += 1
-            self.__id =  type(self).__nb_objects
+            self.id =  type(self).__nb_objects
