ad821dcf9adfebbae73084e5eafe34ee468c67a7
esteban-silvera <estebansilvera000@gmail.com>
2023-10-11 10:43:18 -0300

    2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""solo un rectangulo mi rey"""
+from models.base import Base
+class Rectangle(Base):
+    """solo un rectangulo mi rey"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
