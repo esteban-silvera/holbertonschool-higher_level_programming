15e0d9cc4353784d7f03b065fdb2e6585c3b1048
esteban-silvera <estebansilvera000@gmail.com>
2023-10-14 20:57:04 -0300

    .\square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -39,3 +39,9 @@ class Square(Rectangle):
                     self.x = value
                 elif key == 'y':
                     self.y = value
+
+    def to_dictionary(self):
+        return {
+            "id": self.id, "size": self.width,
+            "x": self.x, "y": self.y
+        }
