dbbdddfc2da86af5f900dcadaccf225376a2c128
esteban-silvera <estebansilvera000@gmail.com>
2023-10-14 19:53:19 -0300

    .\square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,8 +3,8 @@
 from models.rectangle import Rectangle
 class Square(Rectangle):
     """solo un cuadrado mi rey"""
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(width, height, x, y, id)
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
