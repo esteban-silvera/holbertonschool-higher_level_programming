e255d67dfbb2a00f71dffc2058ea2fcfcc1c4d0c
esteban-silvera <estebansilvera000@gmail.com>
2023-10-15 00:21:52 -0300

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """solo un rectangulo mi rey"""
+import json
 class Base:
     """solo un rectangulo mi rey"""
     __nb_objects = 0
@@ -9,3 +10,10 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id =  type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert a list of dictionaries to a JSON string."""
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
