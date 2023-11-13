d6e6de25aa4ae2207ecc069e5e3c5240bc470330
esteban-silvera <estebansilvera000@gmail.com>
2023-11-13 15:21:52 -0300

    fix error

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,16 +1,21 @@
 import mysql.connector
 
+# coment
 cnx = mysql.connector.connect(
   host="localhost",
   user="tu_nombre_de_usuario",
   password="tu_contraseña"
 )
 
+# coment
 cursor = cnx.cursor()
 
+# coment
 cursor.execute("SHOW DATABASES")
 
+# coment
 for db in cursor:
   print(db[0])
 
+# coment
 cnx.close()
