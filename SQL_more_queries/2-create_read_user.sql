5f0e8c1ee493b1e0df9975a7f1b5892d1cc69f57
esteban-silvera <estebansilvera000@gmail.com>
2023-11-18 14:09:30 -0300

    .\2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- coments
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
