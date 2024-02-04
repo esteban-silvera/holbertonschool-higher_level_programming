2717e8ca1472cfcd0ba385936f10774831592ef2
esteban-silvera <estebansilvera000@gmail.com>
2024-02-04 20:53:30 -0300

    init fix

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  const values = fetch('https://hellosalut.stefanbohacek.dev/?lang=fr');
+  values.then(response => response.json()).then(data => {
+    document.getElementById('hello').innerHTML = data.hello;
+  });
+};
+
